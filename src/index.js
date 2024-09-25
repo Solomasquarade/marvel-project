9d525a3617f8f377e5884eb0a9c539a0204573b6
Solomasquarade <104280380+Solomasquarade@users.noreply.github.com>
2024-09-25 22:31:11 +0300

    random char made

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/app/App";
+
 import "./style/style.scss";
+/*
+cd my-app
+npm start
+*/
 
 const container = document.getElementById("root");
 const root = createRoot(container);
